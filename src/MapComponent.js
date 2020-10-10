8fb430e07363d406c9732d70a69f26630d34d2e1
martinejaw <martinezequieljaworski@gmail.com>
2020-10-10 19:50:35 -0300

    Vamos a Saladas

--- src/MapComponent.js
+++ src/MapComponent.js
@@ -7,54 +7,66 @@ import olMap from 'ol/Map';
 import Tile from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 import Zoom from 'ol/control/Zoom';
+import {ScaleLine, defaults as defaultControls} from 'ol/control';
 
 require('ol/ol.css');
 
 export default class MapComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          count:0
-        }
-        this.countPlus = this.countPlus.bind(this);
+  constructor(props) {
+    super(props);
 
-        this.view = new View({
-            center: [-58.986666666667,-27.451388888889],
-            zoom: 30,
-            minZoom: 19,
-            maxZoom: 37,
-            projection: new Projection({
-                code: 'EPSG:4326',
-                units: 'm'
-            })
-        });
-    }
+    this.moveToSaladas = this.moveToSaladas.bind(this);
 
-    componentDidMount() {
-        this.map =  new olMap({
-            view: this.view,
-            controls: [ new Zoom() ],
-            layers: [new Tile({ source: new OSM() })],
-            target: this.refs.mapContainer
-        });
-    }
+  }
 
-    countPlus(){
-      this.setState({
-        count: this.state.count+1
+  componentDidMount() {
+    const view = new View({
+      center: [-58.986666666667, -27.451388888889],
+      zoom: 30,
+      minZoom: 19,
+      maxZoom: 37,
+      projection: new Projection({
+        code: 'EPSG:4326',
+        units: 'm'
+      })
+    });
+
+    var scaleBarSteps = 4;
+    var scaleBarText = true;
+
+    function scaleControl() {
+      const control = new ScaleLine({
+        bar: true,
+        steps: scaleBarSteps,
+        text: scaleBarText,
+        minWidth: 140,
       });
+      return control;
     }
 
-    render() {
-        console.log('-> render App')
-        return (
-            <div>
-              <h4> Mapa </h4>
-              <p> Aqui va algun estado {this.state.count}</p>
-              <Button color='primary' onClick={this.countPlus}> Count++ </Button>
-              <div id="mapContainer" ref="mapContainer"> </div>
-            </div>
-        );
-    }
+
+    this.map = new olMap({
+      view: view,
+      controls: [new Zoom(), scaleControl()],
+      layers: [new Tile({ source: new OSM() })],
+      target: this.refs.mapContainer
+    });
+  }
+
+  moveToSaladas() {
+    this.map.getView().setCenter([-58.625, -28.256]);
+    this.map.getView().setZoom(33);
+  }
+
+  render() {
+    console.log('-> render App')
+    return (
+      <div>
+        <h4> Mapa </h4>
+        <Button color='primary' onClick={this.moveToSaladas}> Vamos a Saladas? </Button>
+        <div id="mapContainer" ref="mapContainer"> </div>
+      </div>
+    );
+  }
 }
 
